32cc9e19b68ceac4a8f224011935cfc70f9e8edc
유재혁 <yjk7454@naver.com>
2026-05-21 09:15:26 +0900

    feat: `1_create_frame.py` - pygame 초기 실행 설정 (화면, 이벤트루프

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init() # 초기화 (반드시 필요)
+
+# 화면 크기 설정
+screen_width = 480 # 가로 크기
+screen_height = 640 # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("YooHyeok Game") # 게임 이름
+
+# 이벤트 루프: 실행되는 동안 발생하는 이벤트를 계속 감지하고 처리
+running = True # 게임 진행중 여부 Flag
+while running:
+  for event in pygame.event.get(): # pygame에서 발생하는 모든 이벤트 추출
+    if event.type == pygame.QUIT: # 게임창의 [X] 버튼을 통한 종료 이벤트
+      running = False
+
+# pygmae 종료
+pygame.quit()
